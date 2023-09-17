a4689d3b0775bfb64fd5267f445720c3c1687f98
Angel Zambrano <angel.zguerrero@gmail.com>
2023-09-17 16:03:39 +0000

    Little fixes

--- apps/scientific_calculator_executor/lib/operator_core/operation.ex
+++ apps/scientific_calculator_executor/lib/operator_core/operation.ex
@@ -2,10 +2,5 @@ defmodule OperatorCore.Operation do
   defstruct operation_name: nil, result: %{}, parameters: %{}, executor: :nil, execution_time: 0
   @type t :: %__MODULE__{operation_name: String.t(), result: Map.t(), parameters: Map.t(), executor: String.t(), execution_time: Number.t()}
 
-  @spec execute(module :: OperatorCore.Operation, parameters :: Map.t) :: OperatorCore.Operation.t()
-  def execute(module, parameters) do
-    start_time = :os.system_time(:millisecond)
-    result = module.execute(parameters)
-    Map.put(result, :execution_time,  :os.system_time(:millisecond) - start_time)
-  end
+
 end
