8276625a224785f8ce418154b0a1b2d87c38e6e6
Angel Zambrano <angel.zguerrero@gmail.com>
2023-09-17 16:03:19 +0000

    Little fixes

--- apps/scientific_calculator_executor/lib/operator_core/operator_core.ex
+++ apps/scientific_calculator_executor/lib/operator_core/operator_core.ex
@@ -1,3 +1,10 @@
 defmodule OperatorCore do
   @callback execute(parameters :: Map.t() ) :: OperatorCore.Operation.t()
+
+  @spec execute(module :: OperatorCore.Operation, parameters :: Map.t) :: OperatorCore.Operation.t()
+  def execute(module, parameters) do
+    start_time = :os.system_time(:millisecond)
+    result = module.execute(parameters)
+    Map.put(result, :execution_time,  :os.system_time(:millisecond) - start_time)
+  end
 end
