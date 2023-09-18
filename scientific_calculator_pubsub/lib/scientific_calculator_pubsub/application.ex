c6ac87142cb54c7c9ec461f4aceba9738928accc
Angel Zambrano <angel.zguerrero@gmail.com>
2023-09-18 00:35:22 +0000

    Some fixes

--- scientific_calculator_pubsub/lib/scientific_calculator_pubsub/application.ex
+++ scientific_calculator_pubsub/lib/scientific_calculator_pubsub/application.ex
@@ -7,10 +7,22 @@ defmodule ScientificCalculatorPubsub.Application do
 
   @impl true
   def start(_type, _args) do
+    redis_host_env = System.get_env("REDIS_HOST")
+    redis_port_env = System.get_env("REDIS_PORT")
+
+    redis_host = case redis_host_env do
+      :nil -> "pendulum-app-redis"
+      _ -> redis_host_env
+    end
+
+    redis_port = case redis_port_env do
+      :nil -> 6379
+      _ -> String.to_integer(redis_port_env)
+    end
     children = [
       {Phoenix.PubSub,
        name: ScientificCalculatorPubsub.Service,
-       adapter: Phoenix.PubSub.Redis, host: "pendulum-app-redis", port: 6379, node_name: System.get_env("NODE")}
+       adapter: Phoenix.PubSub.Redis, host: redis_host, port: redis_port, node_name: System.get_env("NODE")}
     ]
 
     opts = [strategy: :one_for_one, name: ScientificCalculatorPubsub.Supervisor]
